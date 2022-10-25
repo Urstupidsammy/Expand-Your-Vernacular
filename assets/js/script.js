2452885c1eae243bfda5e88c3e001cd4958e38e8
Cherie2 <cwalk280@yahoo.com>
2022-10-24 20:58:38 -0700

    Revised JS, two Apis with fetch fx and a button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
-fetch('https://random-word-api.herokuapp.com/word')
+var randomWordApi = 'https://random-word-api.herokuapp.com/word'
+var generate = document.getElementById("generate");
+
+function generateWord() {
+    fetch(randomWordApi).then(response => response.json()).then(function (words) {
+        console.log(words[0]);
+        getWordDetails(words[0]);
+    })
+}
+
+function getWordDetails(word) {
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            if (response.title) {
+                console.error("No definition found")
+                generateWord();
+            }
+        })
+        .catch(function (err) {
+            console.error("No definition found", err)
+        });
+}
 
 document.getElementById('randomWord')
 
 console.log('test')
+
+generate.addEventListener("click", generateWord);
+//Create elememt audio
+{/* <audio controls>
+  <source src="http://audio.linguarobot.io/en/example-us.mp3" type="audio/mpeg">
+</audio> */}
