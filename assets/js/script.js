798353c6f9fefcd171922907644bc00d1ed74866
TDGLU <tdglukealt@gmail.com>
2022-10-26 03:40:23 -0700

    refractored functions and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,31 @@
-var randomWordApi = 'https://random-word-api.herokuapp.com/word'
-var generate = document.getElementById("generate");
 
+// APIs 
+let randomWordApi = 'https://random-word-api.herokuapp.com/word'
+let wordDefinitonApi = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+
+// Page Elements 
+const generate = document.getElementById("generate");
 const wordBox = document.getElementById("generted-word");
-const definitionBox = document.getElementById("definitionBox")
+const definitionBox = document.getElementById("definitionBox");
+const clearBtn = document.getElementById("clear")
 
+// Function to call the API to get a random word 
 function generateWord() {
     fetch(randomWordApi)
     .then(response => response.json())
     .then(words => {
         console.log(words[0]);
+        // This passes in the word into the getWordDetails function 
         getWordDetails(words[0]);
     })
 }
 
+// This gets more information about the word 
 function getWordDetails(word) {
-    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
+    fetch(wordDefinitonApi + word)
         .then(response => response.json())
         .then(response => {
+            // This checks if the word has a definition 
             if (response.title) {
                 console.error("No definition found")
                 generateWord();
@@ -31,22 +40,25 @@ function getWordDetails(word) {
 
             // Change the word on the page 
             wordBox.textContent = finalWord;
+
             // Change the definition on the page 
             definitionBox.textContent = definition;
-
-
-
         })
         .catch(function (err) {
             console.error("No definition found", err)
         });
 }
 
-document.getElementById('randomWord')
+// Clear function 
+function clearAll() {
+    wordBox.textContent = "Hello!";
+    definitionBox.textContent = "Click below to learn a new word!";
+};
 
-console.log('test')
+clearBtn.addEventListener("click", clearAll);
 
 generate.addEventListener("click", generateWord);
+
 //Create elememt audio
 {/* <audio controls>
   <source src="http://audio.linguarobot.io/en/example-us.mp3" type="audio/mpeg">
