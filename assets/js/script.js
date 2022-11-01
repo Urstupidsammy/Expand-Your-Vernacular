b8114fa3a8bc9498e97542b2799df9d16b92eb66
Cherie2 <cwalk280@yahoo.com>
2022-10-31 19:30:36 -0700

    Revised JS to include audio function, revised readme gif/screenshot/deploy links, revised css with brain image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // APIs 
 let randomWordApi = 'https://random-word-api.herokuapp.com/word'
 let wordDefinitonApi = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+let merriamDictionaryApi ="https://dictionaryapi.com/api/v3/references/collegiate/json/"
 
 // Page Elements by IDs
 const generate = document.getElementById("generate");
@@ -8,6 +9,8 @@ const wordBox = document.getElementById("generted-word");
 const definitionBox = document.getElementById("definitionBox");
 const clearBtn = document.getElementById("clear");
 const date = document.getElementById("date");
+const partOfSpeech = document.getElementById("partOfSpeech");
+const audio = document.getElementById("pronunciation")
 
 // Today's date
 let today = new Date();
@@ -30,10 +33,11 @@ function generateWord() {
 
         // This passes in the word into the getWordDetails function 
         getWordDetails(word);
+        getWordPronunciation(word)
     })
 }
 
-// This gets more information about the word 
+// This gets more information about the word, such as definition/part of speech 
 function getWordDetails(word) {
     fetch(wordDefinitonApi + word)
         .then(response => response.json())
@@ -46,29 +50,65 @@ function getWordDetails(word) {
                 return;
             }
 
-            console.log(response)
-
             // Make first letter in word Capitalized 
             const finalWord = word.charAt(0).toUpperCase() + word.slice(1)
 
             // Store the definition 
             let definition = response[0].meanings[0].definitions[0].definition
 
+            //store the parts of speech(noun,verb etc)
+            let speechParts = response[0].meanings[0].partOfSpeech
+            
             // Change the word on the page 
             wordBox.textContent = finalWord;
 
             // Change the definition on the page 
             definitionBox.textContent = definition;
+
+            //Change the parts of speech on page
+            partOfSpeech.textContent = speechParts;
         })
         .catch(function (err) {
             console.error("No definition found", err)
         });
 }
 
+//Function provides pronunciation for the word through an audio file
+function getWordPronunciation(word) {
+
+    fetch(merriamDictionaryApi + word + "?key=d6970864-1994-4574-8ab7-8d26fa55a301")
+    .then(response => response.json())
+    .then(response => {
+
+        // This checks if the word has a stored pronunciation 
+        if (response.title) {
+            console.error("No Pronunciation found")
+            generateWord();
+            return;
+        }
+
+        //Stores first element of audio file
+        let sound1=response[0].hwi.prs[0].mw.charAt(1);
+        
+        //stores second element of audio file
+        let sound2 = response[0].hwi.prs[0].sound.audio;
+        
+        //Stores complete link to audio file 
+        let pronunciation = "https://media.merriam-webster.com/audio/prons/en/us/mp3/" + sound1 + "/" + sound2 + ".mp3";
+
+        //Places audio file on webpage
+        audio.href=pronunciation;
+    })
+    .catch(function (err) {
+        console.error("No Pronunciation found", err)
+    });
+}
+
 // Clear function 
 function clearAll() {
     wordBox.textContent = "Hello!";
     definitionBox.textContent = "Click below to learn a new word!";
+    partOfSpeech.textContent = ""
 };
 
 // Event listeners
@@ -78,7 +118,4 @@ clearBtn.addEventListener("click", clearAll);
 generate.addEventListener("click", generateWord);
 
 //Future Development Idea
-//Create elememt audio
-{/* <audio controls>
-  <source src="http://audio.linguarobot.io/en/example-us.mp3" type="audio/mpeg">
-</audio> */}
+//Create visual representation of word through a photo or short video
